042d86867a9643b023ca83726075ff9f3b63df07
Leonardo do Nascimento <leonardo3.nascimento@gmail.com>
2023-05-27 15:51:14 -0300

    dev: uploads e servindo arquivos estáticos com o backend

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,9 +2,13 @@ import 'dotenv/config'
 
 import cors from '@fastify/cors'
 import jwt from '@fastify/jwt'
+import multipart from '@fastify/multipart'
+import staticFiles from '@fastify/static'
 import fastify from 'fastify'
+import { resolve } from 'node:path'
 import { authRoutes } from './routes/auth'
 import { memoriesRoutes } from './routes/memories'
+import { uploadRoutes } from './routes/upload'
 
 const app = fastify()
 app.register(cors, {
@@ -13,8 +17,15 @@ app.register(cors, {
 app.register(jwt, {
   secret: process.env.AUTH_SECRET!,
 })
-app.register(memoriesRoutes)
+app.register(multipart)
+app.register(staticFiles, {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads',
+})
+
 app.register(authRoutes)
+app.register(uploadRoutes)
+app.register(memoriesRoutes)
 
 app
   .listen({
